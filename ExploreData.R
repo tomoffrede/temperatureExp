2881c8d04ee098c74d8f388856c711c9fa60a610
Tom Offrede <tom.offrede@gmail.com>
2022-05-17 12:12:30 +0200

    fixed some things

--- ExploreData.R
+++ ExploreData.R
@@ -154,8 +154,8 @@ folder2 <- "C:/Users/tomof/Documents/1HU/ExperimentTemperature/Data/FWR/"
 
 files <- list.files(folder2)
 
-dat <- data.frame(matrix(nrow=0, ncol=4))
-names(dat) <- c("speaker", "frame", "ROI", "temperature")
+dat <- data.frame(matrix(nrow=0, ncol=5))
+names(dat) <- c("speaker", "frame", "ROI", "temperature", "Tz")
 
 for(f in files){
   a <- read.table(paste0(folder2, f), sep = "\t", header=TRUE)
@@ -163,22 +163,42 @@ for(f in files){
   names(a) <- gsub("....C.", "", names(a))
   a <- a %>% mutate_at(names(a), as.numeric)
   
-  temp <- data.frame(matrix(nrow=0, ncol=4))
-  names(temp) <- c("speaker", "frame", "ROI", "temperature")
+  temp <- data.frame(matrix(nrow=0, ncol=5))
+  names(temp) <- c("speaker", "frame", "ROI", "temperature", "Tz") # Tz is the temperature in z scores
   
   for(i in 1:ncol(a)){
     frame <- substr(f, 5, 6)
     frame <- gsub("\\.", "", frame)
     sp <- paste0(substr(f, 1, 3), "-", substr(names(a[i]), nchar(names(a[i])), nchar(names(a[i]))))
     
-    o <- data.frame(sort(a[,i], decreasing=TRUE)) %>%
-      slice(1:(nrow(o)*0.1)) # if you want a different amount of pixels (instead of 10%), just change the 0.1 here
-    temp[nrow(temp)+1,] <- c(sp, frame, gsub("\\..*", "", names(a[i])), as.numeric(mean(o[,1])))
+    o <- sort(a[,i], decreasing=TRUE)
+    o <- o[1:(length(o)*0.1)] # if you want a different amount of pixels (instead of 10%), just change the 0.1 here
+    temp[nrow(temp)+1,] <- c(sp, frame, gsub("\\..*", "", names(a[i])), as.numeric(mean(o)), NA)
     temp$temperature <- as.numeric(temp$temperature)
-    if(i == ncol(a)){
-      temp[nrow(temp)+1,] <- c(sp, frame, "Face", mean(temp$temperature))
+    if(i < ncol(a)){
+      nextsp <- paste0(substr(f, 1, 3), "-", substr(names(a[i+1]), nchar(names(a[i+1])), nchar(names(a[i+1]))))
+      if(nextsp != sp){ # if this is the last ROI of the current speaker for this frame, calculate the mean temperature of all ROIs
+        temp <- temp %>%
+          mutate(Tz = (temperature - mean(temperature)) / sd(temperature)) %>%
+          filter(abs(Tz) < 2)
+        temp[nrow(temp)+1,] <- c(sp, frame, "Face", mean(temp$temperature[temp$speaker==sp]), NA)
+        }
+      } else if(i == ncol(a)){
+          temp <- temp %>%
+            mutate(Tz = (temperature - mean(temperature)) / sd(temperature)) %>%
+            filter(abs(Tz) < 2)
+          temp[nrow(temp)+1,] <- c(sp, frame, "Face", mean(temp$temperature[temp$speaker==sp]), NA)
+      }
     }
-  }
-  
   dat <- rbind(dat, temp)
 }
+
+dat <- dat %>%
+  mutate_at(c("speaker", "ROI"), as.factor) %>%
+  mutate_at("frame", as.integer) %>%
+  mutate_at(c("temperature", "Tz"), as.numeric)
+
+ggplot(dat, aes(frame, temperature))+
+  geom_point()+
+  geom_line()+
+  facet_grid(speaker~ROI)
