ff2ef3e19408c1b7ea0b35a8aa90c7e2401042b8
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2023-02-22 17:05:05 +0100

    .

---
+++ ManipulateTextGrids.R
@@ -0,0 +1,44 @@
+# Tom Offrede
+# Rename tiers from textgrids
+
+library(rPraat)
+
+folderTG <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/compare/"
+
+# Rename tiers depending on their name
+
+TGv <- list.files(folderTG, "sil.TextGrid") # v for vector
+
+# for(t in TGv){
+#   tg <- tg.read(paste0(folderTG, t), encoding=detectEncoding(paste0(folderTG, t)))
+#   if(tg.getTierName(tg, 1) == "speaker1"){
+#     tg <- tg.setTierName(tg, 1, "speakerA")
+#   }
+#   if(tg.getTierName(tg, 2) == "speaker2"){
+#     tg <- tg.setTierName(tg, 2, "speakerB")
+#   }
+#   tg.write(tg, paste0(folderTG, t))
+# }
+
+for(t in TGv){
+  tg <- tg.read(paste0(folderTG, t), encoding=detectEncoding(paste0(folderTG, t)))
+  if(grepl("-A-", t)){
+    tg <- tg.setTierName(tg, 1, "silenceA")
+  }
+  if(grepl("-B-", t)){
+    tg <- tg.setTierName(tg, 1, "silenceB")
+  }
+  tg.write(tg, paste0(folderTG, t))
+}
+
+# See if they all have the right name now
+
+tiers <- data.frame(matrix(nrow=0, ncol=3))
+names(tiers) <- c("file", "tier1", "tier2")
+
+for(t in TGv){
+  tg <- tg.read(paste0(folderTG, t), encoding=detectEncoding(paste0(folderTG, t)))
+  tiers[nrow(tiers)+1,] <- c(t,
+                             tg.getTierName(tg, 1),
+                             tg.getTierName(tg, 2))
+}
