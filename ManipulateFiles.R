dddac855b820473732360de6d5eae16637c36c57
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2023-03-14 18:09:25 +0100

    added chunk to copy file following file name of other files

--- ManipulateFiles.R
+++ ManipulateFiles.R
@@ -5,20 +5,46 @@
 library(tidyverse)
 library(tuneR)
 
-folder <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/compare/"
-folder2 <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/compare/silTextGrids/"
+folder <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/HAG/compare/"
+folder2 <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/TestF0Extraction/AllWAV/NewExtraction/FXO/"
 folderAllTG <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/AllTextGrids/"
 folderAllW <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/AllWav/"
-folderAllWF <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/compare/"
+folderAllWF <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/SpeechData/TestF0Extraction/AllWAV/"
 dyads <- c("AML", "FWR", "FXO", "HAG", "HBR", "HUJ", "KDA", "KPB", "MJG", "NLO", "OAL", "OXQ", "QRT", "SGB", "SUK", "TTN", "TTY", "VDE", "ZNV")
+dyads <- c("FXO", "NLO", "KDA", "QRT")
 
-# Copy all files from one file to another
+# Copy all files from one folder to another
 
-# og <- list.files(folder, "sil.TextGrid")
-# for(o in og){
-#   file.copy(from = paste0(folder, o),
-#             to = paste0(folder2, o))
-# }
+og <- list.files(folder, "OverlapSp.TextGrid")
+og <- og[!grepl("Overlap", og)]
+for(o in og){
+  file.copy(from = paste0(folder, o),
+            to = paste0(folder2, o))
+}
+
+# Copy each TextGrid twice into the same folder, each time taking the name of the corresponding wav file (there are 2 wav files per TextGrid)
+
+# tg <- list.files(folder, "^[A-Z]{3}-(D|L)[0-9]\\.TextGrid$")
+tg <- list.files(folder, "f0\\.TextGrid$")
+w <- list.files(folder, "wav")
+wA <- w[grepl("-A", w)]
+wB <- w[grepl("-B", w)]
+
+files0 <- cbind(wA, wB)
+files <- data.frame(cbind(files0, tg)) %>%  
+  mutate(worked1 = ifelse(substr(wA, 1, 6) == substr(wB, 1, 6), "worked!", "NO!!!"),
+         worked2 = ifelse(substr(wA, 1, 6) == substr(tg, 1, 6), "worked!", "NO!!!"))
+table(files$worked1)
+table(files$worked2)
+
+for(i in 1:nrow(files)){
+  tgA <- gsub("wav", "TextGrid", files$wA[i])
+  tgB <- gsub("wav", "TextGrid", files$wB[i])
+  for(t in c(tgA, tgB)){
+    file.copy(from = paste0(folder, files$tg[i]),
+              to = paste0(folder, t), overwrite = TRUE)
+  }
+}
 
 # # Create folders
 # 
@@ -26,15 +52,15 @@ dyads <- c("AML", "FWR", "FXO", "HAG", "HBR", "HUJ", "KDA", "KPB", "MJG", "NLO",
 #   dir.create(paste0(folder, d))
 # }
 
-# # Copy files into their appropriate folder
+# # Copy files from a general folder into their appropriate folder
 
-t <- list.files(folderAllWF, "wav")
+t <- list.files(folder2, "OverlapSp2")
 
 for(f in t){
   for(d in dyads){
-    folderCurrentDyad <- paste0(folder, d, "/")
+    folderCurrentDyad <- paste0(folder2, d, "/")
     if(grepl(d, f)){
-      file <- paste0(folderAllWF, f, "/")
+      file <- paste0(folder2, f, "/")
       file.copy(file, folderCurrentDyad)
     }
   }
@@ -43,11 +69,11 @@ for(f in t){
 # Copy files from their individual folder into a folder with all speakers
 
 for(d in dyads){
-  folderC <- paste0(folder, d, "/")
-  files <- list.files(folderC, "txt")
+  folderC <- paste0(folder2, d, "/")
+  files <- list.files(folderC, "OverlapSp.TextGrid")
   for(f in files){
     file <- paste0(folderC, f, "/")
-    file.copy(file, folderAllWF)
+    file.copy(file, folder2)
   }
 }
 
@@ -120,10 +146,11 @@ hist(s$lengthL)
 
 # Option 2
 
-files <- list.files(folderAllWF, "TextGrid")
+files <- list.files(folder, "OverlapSp.TextGrid")
+# files <- files[!grepl("OG", files)]
 
 for(f in files){
-  newName <- gsub("TextGrid", "-OG.TextGrid", f)
-  file.rename(from = paste0(folderAllWF, f),
-              to = paste0(folderAllWF, newName))
+  newName <- gsub("-OverlapSp", "", f)
+  file.rename(from = paste0(folder, f),
+              to = paste0(folder, newName))
 }
