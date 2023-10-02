9e2e4d872e508c5a62469a5c1c3983ab7855ab88
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2023-10-02 15:05:53 +0200

    .

--- scripts/Preprocessing-Temp.R
+++ scripts/Preprocessing-Temp.R
@@ -4,122 +4,135 @@
 library(tidyverse)
 
 folderTXT <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/TempData/Extraction-Official/"
+folderNew <- "C:/Users/offredet/Documents/1HU/ExperimentTemperature/Data/TempData/Extraction-Official/onlyTable/"
 `%!in%` <- Negate(`%in%`)
 
 files <- list.files(folderTXT, "\\.txt")
+onlyDiapix <- c("AML", "FWR")
 
-dat <- data.frame(matrix(nrow=0, ncol=4))
-names(dat) <- c("speaker", "timeStamp", "ROI", "temperature")
-
+dat <- data.frame(matrix(nrow=0, ncol=5))
+names(dat) <- c("speaker", "timeStamp", "ROI", "temperature", "task")
 
 for(f in files){
   lines <- readLines(paste0(folderTXT, f))
   timeStamp <- substr(str_split(lines[6], fixed("\t"))[[1]][[2]], 1, 8) # splits the line that contains the time stamp and gets the 2nd value (i.e. the time stamp), and then save only 1st to 8th characters, i.e. no milisecond info
+  if(substr(f, 1, 3) %in% onlyDiapix){
+    task <- "Diapix"
+  } else{
+    task <- ifelse(grepl("1", str_split(lines[3], fixed("\t"))[[1]][[2]]), # get the name of the thermal video file and check if it contains a "1" or not, which indicates if it was recorded during the Lists or Diapix task
+                        "Diapix",
+                        "Lists")}
   
-  a <- read.table(paste0(folderTXT, f), sep = "\t", header=TRUE, fileEncoding = readr::guess_encoding(paste0(folderTXT, f))$encoding[1])
-  a <- a[,grepl("C", names(a)) & !grepl("px", names(a))]
+  writeLines(lines[21:length(lines)], paste0(folderNew, f))
+  
+  a <- read.table(paste0(folderNew, f), sep = "\t", header=TRUE, fileEncoding = readr::guess_encoding(paste0(folderNew, f))$encoding[1]) |> 
+    select(contains("C") & !contains("px")) |> 
+    mutate_all(as.numeric)
   names(a) <- gsub("\\.\\.\\.\\.C\\.", "", names(a))
-  # names(a) <- gsub("....C.", "", names(a))
-  a <- a %>% mutate_at(names(a), as.numeric)
-  if(nchar(f) == 9){ # if it's frame [1-9], put a zero before it, so that all files have 10 characters (counting with ".txt")
-    f <- gsub("-", "-0", f)
-  }
   
   tempA <- data.frame(matrix(nrow=0, ncol=4))
-  names(tempA) <- c("speaker", "frame", "ROI", "temperature")
+  names(tempA) <- c("speaker", "timeStamp", "ROI", "temperature")
   tempB <- data.frame(matrix(nrow=0, ncol=4))
-  names(tempB) <- c("speaker", "frame", "ROI", "temperature")
-  # w <- NA
+  names(tempB) <- c("speaker", "timeStamp", "ROI", "temperature")
+
   for(i in 1:ncol(a)){
-    # if(substr(names(a[i]), nchar(names(a[i])), nchar(names(a[i]))) %!in% c("A", "B") || substr(names(a[i]), 1, 1) %!in% c("F", "E", "N", "C", "M")){ # some columns were named wrong and contain only numbers. Here I try to correct them.
-    #   wrong[nrow(wrong)+1,] <- c(f, names(a[i]))
-    # }
-    
-    frame <- substr(f, 5, 6)
     sp <- paste0(substr(f, 1, 4), substr(names(a[i]), nchar(names(a[i])), nchar(names(a[i]))))
     
     o <- sort(a[,i], decreasing=TRUE)
     o <- o[1:(length(o)*0.1)] # if you want a different amount of pixels (instead of 10%), just change the 0.1 here
     
     if(substr(sp, nchar(sp), nchar(sp)) == "A"){
-      tempA[nrow(tempA)+1,] <- c(sp, frame, gsub("\\..*", "", names(a[i])), as.numeric(mean(o)))
+      tempA[nrow(tempA)+1,] <- c(sp, timeStamp, gsub("\\..*", "", names(a[i])), as.numeric(mean(o)))
     }
     if(substr(sp, nchar(sp), nchar(sp)) == "B"){
-      tempB[nrow(tempB)+1,] <- c(sp, frame, gsub("\\..*", "", names(a[i])), as.numeric(mean(o)))
+      tempB[nrow(tempB)+1,] <- c(sp, timeStamp, gsub("\\..*", "", names(a[i])), as.numeric(mean(o)))
     }
   }
   
   temp <- list(tempA, tempB)
-  
   for(t in 1:length(temp)){
-    if(nrow(temp[[t]]) > 1){
-      temp[[t]]$temperature <- as.numeric(temp[[t]]$temperature)
-      temp[[t]][nrow(temp[[t]])+1,] <- c(unique(temp[[t]]$speaker), frame, "Face", mean(temp[[t]]$temperature, na.rm=TRUE))
-    }
+    temp[[t]]$temperature <- as.numeric(temp[[t]]$temperature)
+    if("E1" %in% temp[[t]]$ROI & "E2" %in% temp[[t]]$ROI){
+      temp[[t]][nrow(temp[[t]])+1,] <- c(unique(temp[[t]]$speaker),
+                                         timeStamp,
+                                         "E",
+                                         mean(c(temp[[t]]$temperature[temp[[t]]$ROI=="E1"], temp[[t]]$temperature[temp[[t]]$ROI=="E2"])))
+    } else{temp[[t]]$ROI[substr(temp[[t]]$ROI, 1, 1)=="E"] <- "E"}
+    temp[[t]]$temperature <- as.numeric(temp[[t]]$temperature)
+    if("C1" %in% temp[[t]]$ROI & "C2" %in% temp[[t]]$ROI){
+      temp[[t]][nrow(temp[[t]])+1,] <- c(unique(temp[[t]]$speaker),
+                                         timeStamp,
+                                         "C",
+                                         mean(c(temp[[t]]$temperature[temp[[t]]$ROI=="C1"], temp[[t]]$temperature[temp[[t]]$ROI=="C2"])))
+    } else{temp[[t]]$ROI[substr(temp[[t]]$ROI, 1, 1)=="C"] <- "C"}
+    
+    temp[[t]] <- temp[[t]] |> 
+      filter(ROI %!in% c("E1", "E2", "C1", "C2"))
   }
   
-  temp0 <- rbind(temp[[1]], temp[[2]])
+  temp0 <- rbind(temp[[1]], temp[[2]]) |> 
+    mutate(task = task) |> 
+    mutate_at(c("speaker", "ROI", "task"), as.factor) |> 
+    mutate_at(c("temperature"), as.numeric)
   dat <- rbind(dat, temp0)
-  
-  # f <- paste0(substr(f, 1, 4), as.numeric(frame) + 1, ".txt")
 }
 
-dat <- dat %>%
-  mutate_at(c("speaker", "ROI"), as.factor) %>%
-  mutate_at("frame", as.integer) %>%
-  mutate_at(c("temperature"), as.numeric)
+# datSave <- dat
+# dat <- datSave
+
+dat$timeStamp <- as.POSIXct(dat$timeStamp, format="%H:%M:%S")
+
+dat <- dat |>
+  filter(ROI != "M") |> # comment this line out if you want to keep the ROI mouth
+  mutate(ROI = case_when(
+    ROI=="N" ~ "Nose",
+    ROI=="E" ~ "Eyes",
+    ROI=="M" ~ "Mouth",
+    ROI=="C" ~ "Cheeks",
+    ROI=="F" ~ "Forehead"
+  )) |> 
+  mutate_at(c("speaker", "ROI", "task"), as.factor) |>
+  mutate_at(c("temperature"), as.numeric) |> 
+  group_by(task, speaker, ROI) |> 
+  mutate(startTime = min(timeStamp),
+         time = as.numeric((timeStamp - startTime)/60)) |> # store passage of time in minutes
+  ungroup() |> 
+  select(-startTime)
   
-# There's a very high (.97+) correlation between C1 and C2 and between E1 and E2
-
-c <- dat %>%
-  filter(ROI %in% c("C1", "C2", "E1", "E2")) %>% 
-  mutate(ROI = ifelse(ROI %in% c("C1", "C2"), "C", "E")) %>% 
-  group_by(speaker, frame, ROI) %>% 
-  mutate(newTemp = mean(temperature, na.rm=TRUE)) %>% 
-  ungroup() %>% 
-  filter(!duplicated(paste(speaker, frame, ROI))) %>% 
-  select(-temperature) %>% 
-  rename(temperature=newTemp)
-
-dat <- dat %>% 
-  filter(ROI %!in% c("C1", "C2", "E1", "E2"))
-
-dat <- rbind(dat, c)
-
 # Join metadata (which was cleaned in Preprocessing-Speech):
 
 load(paste0(here::here(), "/data/metadata-clean.RData"))
 
-m <- m %>% 
-  pivot_longer(realTempPre:realTempPost, names_to = "stage", values_to = "realTemp") %>% 
+m <- m |> 
+  pivot_longer(realTempPre:realTempPost, names_to = "stage", values_to = "realTemp") |> 
   mutate(stage = ifelse(stage == "realTempPre", "beginning", ifelse(stage == "realTempDuring", "middle", "ending")))
 
-dat <- dat %>% 
-  group_by(speaker) %>% 
-  mutate(stageFrame = frame / max(frame),
-         stage = ifelse(stageFrame <= 0.33, "beginning", ifelse(stageFrame >= 0.66, "ending", "middle"))) %>% 
-  ungroup() %>% 
-  select(-stageFrame) %>% 
+dat <- dat |> 
+  group_by(speaker, task) |> 
+  mutate(stageTime = time / max(time),
+         stage = ifelse(stageTime <= 0.33, "beginning", ifelse(stageTime >= 0.66, "ending", "middle"))) |> 
+  ungroup() |> 
+  select(-stageTime) |> 
   mutate(tempDiff = NA)
 
 dat0 <- dat
 # dat <- dat0
 
 for(s in unique(dat$speaker)){
-  for(f in dat$frame){
+  for(t in dat$time){
     for(r in dat$ROI){
       currentTemp <- as.numeric(dat$temperature[dat$speaker==s &
-                                                  dat$frame==f &
+                                                  dat$time==t &
                                                   dat$ROI==r])
       otherTemp <- as.numeric(dat$temperature[dat$speaker != s &
                                                 substr(dat$speaker, 1, 3) == substr(s, 1, 3) &
-                                                dat$frame==f &
+                                                dat$time==t &
                                                 dat$ROI==r])
       if(!purrr::is_empty(currentTemp)){
         if(!any(is.na(currentTemp))){
           if(!purrr::is_empty(otherTemp)){
             if(!any(is.na(otherTemp))){
-              dat$tempDiff[dat$speaker==s & dat$frame==f & dat$ROI==r] <- as.numeric(abs(currentTemp - otherTemp))
+              dat$tempDiff[dat$speaker==s & dat$time==t & dat$ROI==r] <- as.numeric(abs(currentTemp - otherTemp))
             }
           }
         }
@@ -130,4 +143,4 @@ for(s in unique(dat$speaker)){
 
 dat <- merge(dat, m, by=c("speaker", "stage"))
 
-save(dat, file=paste0(here::here(), "/data/tempData-wuppertal.RData"))
+save(dat, file=paste0(here::here(), "/data/tempData.RData"))
--- scripts/leftOvers_Preprocessing.R
+++ scripts/leftOvers_Preprocessing.R
@@ -36,3 +36,35 @@ for(f in files){
 
 ####
 
+# check all ROIs from all files and see if they're all named properly
+
+roi <- data.frame(matrix(nrow=0, ncol=2))
+names(roi) <- c("file", "ROI")
+for(f in files){
+  lines <- readLines(paste0(folderTXT, f))
+  writeLines(lines[21:length(lines)], paste0(folderNew, f))
+  
+  a <- read.table(paste0(folderNew, f), sep = "\t", header=TRUE, fileEncoding = readr::guess_encoding(paste0(folderNew, f))$encoding[1]) |> 
+    select(contains("C") & !contains("px")) |> 
+    mutate_all(as.numeric)
+  names(a) <- gsub("\\.\\.\\.\\.C\\.", "", names(a))
+  for(col in names(a)){
+    roi[nrow(roi)+1,] <- c(f, col)
+  }
+}
+
+####
+
+# check if the temperature unit is celsius in all files
+
+celsius <- data.frame(matrix(nrow=0, ncol=2))
+names(celsius) <- c("file", "ok")
+for(f in files){
+  lines <- readLines(paste0(folderTXT, f))
+  if(grepl("°C", str_split(lines[13], fixed("\t"))[[1]][[1]])){
+    celsius[nrow(celsius)+1,] <- c(f, "ok")
+  } else{celsius[nrow(celsius)+1,] <- c(f, "no!!!")}
+}
+
+####
+
